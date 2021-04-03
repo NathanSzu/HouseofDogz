20dffa9b632f882149812124d01587d40b96151f
Nathan Szurek <nathan.szurek@yahoo.com>
2021-04-03 16:09:02 -0500

    Added company logo set

--- src/components/BootstrapNavbar.js
+++ src/components/BootstrapNavbar.js
@@ -1,26 +1,24 @@
 import React from 'react'
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
-import NavDropdown from 'react-bootstrap/NavDropdown'
+import logo from '../utils/assets/logo192.png'
 
 export default function BootstrapNavbar() {
     return (
-        <Navbar bg="light" expand="lg">
-            <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="mr-auto">
-                    <Nav.Link href="#home">Home</Nav.Link>
-                    <Nav.Link href="#link">Link</Nav.Link>
-                    <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                        <NavDropdown.Divider />
-                        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                    </NavDropdown>
-                </Nav>
-            </Navbar.Collapse>
-        </Navbar>
+        <nav>
+            <Navbar bg="light" expand="lg">
+                <Navbar.Brand href="/"><img className='nav-logo' alt='logo' src={logo}></img></Navbar.Brand>
+                <Navbar.Brand href="/">House of Dogz</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="ml-auto">
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#link">Info</Nav.Link>
+                        <Nav.Link href="#link">Customer Login</Nav.Link>
+                        <Nav.Link href="#link">Create Account</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+        </nav>
     )
 }
